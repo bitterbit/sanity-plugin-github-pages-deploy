aa59f1cc53a2ab9af6d77947b87f75f1f5a21c4a
galtashma <galtashma@gmail.com>
2021-10-06 23:15:20 +0300

    add sanity ui

--- src/panel/dashboard.js
+++ src/panel/dashboard.js
@@ -1,48 +1,61 @@
 import config from "config:github-pages-deploy";
 import React from 'react'
 
+import {
+    Container,
+    Card,
+    Heading,
+    Stack,
+    Button,
+    Inline,
+} from '@sanity/ui'
+
 console.log('config', config);
 
-class Button extends React.Component {
+class DeployButton extends React.Component {
     /**
      * @param repo of shape owner/name
      */
     triggerDeployment(repo) {
-        console.log('trigger deployment', repo);
         let xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = function() {
-            console.log('state change');
-        }
-
         xhr.open('POST', `https://api.github.com/repos/${repo}/dispatches`, true);
         xhr.setRequestHeader('Authorization', `Bearer ${config.token}`);
         xhr.send('{"event_type": "Deploy triggered from Sanity Studio"}');
     }
 
+
     render() {
         return (
-            <button onClick={() => this.triggerDeployment(this.props.repo)}>Deploy {this.props.repo}</button>
+            <Button
+                fontSize={[2, 2, 3]}
+                onClick={() => this.triggerDeployment(this.props.repo)}
+                mode="ghost"
+                padding={[3, 3, 4]}
+                text={`Deploy ${this.props.repo}`}
+                key={this.props.repo}
+            />
         )
     }
 }
 
 class Dashboard extends React.Component {
-    render() {
 
-        console.log('repositories', config.repositories);
-        const buttons =  config.repositories.map((repo) => {
-            return (<Button repo={repo} />)
+    render() {
+        const buttons = config.repositories.map((repo, index) => {
+            return (<DeployButton repo={repo} key={index} />)
         });
 
-        console.log('buttons?', buttons);
-
         return (
-            <div>
-                <p>This is a blank slate for you to build on.</p>
-                <p>Tools are just React components!</p>
-                {buttons}
-            </div>
+            <Container width={3}>
+                <Card padding={4}>
+                    <Stack space={3}>
+                        <Heading marginBottom={2}>Github Pages Deploy</Heading>
+                        <Inline space={[3, 3, 4]}>
+                            {buttons}
+                        </Inline>
+                    </Stack>
+                </Card>
+            </Container>
         )
     }
 }
